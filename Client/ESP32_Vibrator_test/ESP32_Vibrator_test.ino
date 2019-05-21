ca2cec678fba01a93a24f66ac83df09c9937ba30
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2019-05-22 01:22:18 +0200

    vibrator test changes

--- Client/ESP32_Vibrator_test/ESP32_Vibrator_test.ino
+++ Client/ESP32_Vibrator_test/ESP32_Vibrator_test.ino
@@ -1,18 +1,18 @@
 int motorPin = 15;
- 
+
 void setup() {
- 
+
   pinMode(motorPin, OUTPUT);
- 
+
 }
- 
+
 void loop() {
- 
-  digitalWrite(motorPin, 100);
-  delay(1000);
-  digitalWrite(motorPin, 200);
-  delay(1000);
+  digitalWrite(motorPin, HIGH);
+  delay(200);
   digitalWrite(motorPin, LOW);
-  delay(1000);
- 
+  delay(200);
+  digitalWrite(motorPin, HIGH);
+  delay(200);
+  digitalWrite(motorPin, LOW);
+  delay(500);
 }
